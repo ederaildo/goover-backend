7cf3bddbeb6c854341f16b08feed08a8fabfa40a
Ederaildo Fontes <ederaildo@gmail.com>
2016-07-27 16:27:05 -0300

    Nova versao com Keystone

--- modules/programas/client/services/programas.veiculosResource.service.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('programas').factory('programasVeiculosResources', function ($resource) {
-    return $resource('modules/programas/client/config/programas.list.veiculos.json');
-});
